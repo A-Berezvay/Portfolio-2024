416f6928b7ef9c230e3673430bf3914f8facbf1c
A-Berezvay <berezvay.albert@gmail.com>
2024-03-23 17:56:56 +0100

    Added content to About and Contact page

--- resources/app.js
+++ resources/app.js
@@ -7,6 +7,7 @@ const menuCloseButton = document.getElementById('close-button');
 const navLinks = document.querySelector('header nav ul');
 
 menuButton.addEventListener('click', function() {
+    console.log('Menu button clicked')
     navLinks.classList.add('show-menu');
     menuCloseButton.style.display = 'block';
 });
@@ -46,5 +47,30 @@ const textLoad = () => {
 
 textLoad();
 setInterval(textLoad, 12000);
+
+// SLIDER
+
+let currentImageIndex = 0;
+const images = document.querySelectorAll('.slider-img');
+const totalImages = images.length;
+
+images[currentImageIndex].classList.add('active'); // Initialize the first image as active
+
+function changeImage(nextIndex) {
+  images[currentImageIndex].classList.remove('active'); // Remove active class from current image
+  images[nextIndex].classList.add('active'); // Add active class to next image
+  currentImageIndex = nextIndex;
+}
+
+document.getElementById('nextBtn').addEventListener('click', function() {
+  const nextIndex = (currentImageIndex + 1) % totalImages;
+  changeImage(nextIndex);
+});
+
+document.getElementById('prevBtn').addEventListener('click', function() {
+  const nextIndex = (currentImageIndex - 1 + totalImages) % totalImages;
+  changeImage(nextIndex);
+});
+
   
   
